02e916fff383196435983363c3a7e3f6ae68fbc8
Alan Deng <119882344+AlanDeng7@users.noreply.github.com>
2023-03-23 21:34:58 -0400

    Update Schema_Group3.sql

--- ERD_Schema/Schema/Schema_Group3.sql
+++ ERD_Schema/Schema/Schema_Group3.sql
@@ -1,16 +1,12 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
-
 CREATE TABLE "campaign" (
-    "cf_id" int(64)   NOT NULL,
-    "contact_id" int(64)   NOT NULL,
+    "cf_id" int   NOT NULL,
+    "contact_id" int   NOT NULL,
     "company_name" varchar(64)   NOT NULL,
     "description" varchar(64)   NOT NULL,
-    "goal" float(64)   NOT NULL,
-    "pledged" float(64)   NOT NULL,
+    "goal" float   NOT NULL,
+    "pledged" float   NOT NULL,
     "outcome" varchar(64)   NOT NULL,
-    "backers_count" int(64)   NOT NULL,
+    "backers_count" int   NOT NULL,
     "country" varchar(64)   NOT NULL,
     "currency" varchar(64)   NOT NULL,
     "launched_date" date   NOT NULL,
@@ -31,7 +27,7 @@ CREATE TABLE "category" (
 );
 
 CREATE TABLE "contacts" (
-    "contact_id" int(64)   NOT NULL,
+    "contact_id" int   NOT NULL,
     "first_name" varchar(64)   NOT NULL,
     "last_name" varchar(64)   NOT NULL,
     "email" varchar(128)   NOT NULL,
@@ -56,4 +52,9 @@ REFERENCES "category" ("category_id");
 
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
 REFERENCES "subcategory" ("subcategory_id");
+
+SELECT * FROM campaign
+SELECT * FROM category
+SELECT * FROM contacts
+SELECT * FROM subcategory
 
